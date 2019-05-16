1ecb2b42608c56cec476f5af8700d1cf63e533f0
Dima <48024372+Dima1122@users.noreply.github.com>
2019-05-16 22:15:48 +0300

    minor changes

--- app.py
+++ app.py
@@ -2,50 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import flask
-from flask import request, url_for, make_response, render_template, jsonify, redirect
+from flask import request,url_for,make_response,render_template,jsonify, redirect
 from flask_wtf import Form
 from wtforms import TextAreaField, SubmitField
 from wtforms.validators import Required
-from processing.preprocessing import get_raw_df
-from processing.natashenka import recognize_all
+
 
 
 app = flask.Flask(__name__)
-app.config['SECRET_KEY'] = "SOADISAHOIDLHWNEOIASLDNXOJASKBDOALSXANSLKXAIDNO"
-
+app.config['SECRET_KEY']="SOADISAHOIDLHWNEOIASLDNXOJASKBDOALSXANSLKXAIDNO"
 
 class TextForm1(Form):
-    enter = TextAreaField('Enter your text', validators=[Required()])
+    enter = TextAreaField('Enter your text',validators=[Required()],  render_kw={"rows": 18, "cols": 150})
     submit = SubmitField('Analyze')
 
-
 class TextForm2(Form):
-    enter = TextAreaField('Here will be yput anonimized text', validators=[Required()])
-
+    enter = TextAreaField('Here will be your anonimized text',validators=[Required()], render_kw={"rows": 18, "cols": 150})
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-
 @app.route('/anonimize_text', methods=['GET', 'POST'])
 def home():
-    enter1 = None
+    enter1= None
     enter2 = None
     form1 = TextForm1()
     form2 = TextForm2()
-    # if form1.validate_on_submit():
+    #if form1.validate_on_submit():
     if request.method == "POST":
-        form2.enter.data = recognize_all(form1.enter.data)
-        # return redirect(url_for('anonimize'))
-    return render_template("index.html",
-                           form=[form1, form2],
-                           enter=[enter1, enter2])
+        form2.enter.data=anonimize(form1.enter.data)
+        #return redirect(url_for('anonimize'))
+    return render_template ("index.html",form=[form1,form2],enter= [enter1,enter2])
 
-
-# @app.route('/anonimized')
+#@app.route('/anonimized')
 def anonimize(s):
     return "The text '{}' will be anonimized".format(s)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9995, debug=True)
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=9995,debug=True)
+
